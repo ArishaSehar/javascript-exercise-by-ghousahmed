d1fe86aeff8ad684c749fd9896b174da2cd24477
arishasehar <arshsehar66@gmail.com>
2024-05-28 12:45:32 +0500

    javascript-exercise

---
+++ chapter-1/app.js
@@ -0,0 +1,23 @@
+// question-1
+// alert("Hello, Where have you been upto!");
+
+// question-2
+// alert("Error! Please Enter a Valid Password");
+
+// question-3
+// alert(" Welcome to JS Land...\n Happy Coding!");
+
+// question-4
+// alert("Welcome to JS Land.");
+// alert("Happy Coding! \n Prevent this page from creating additional dialouges");
+
+// question-5
+// alert("Hello user");
+// console.log("Hello.. I can run js through my web browser's console");
+
+//question-6
+// alert("Hello,Good Day! Please click ok to see the page");
+
+// question-7
+/*Practice placement of <script></script> element in 
+following sections of your project */
